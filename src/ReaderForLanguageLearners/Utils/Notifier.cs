c8cea9cfeb1836b1823ec874f1c57cd37fec5d81
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-09-21 12:03:26 +0300

    Utils init commit

---
+++ src/ReaderForLanguageLearners/Utils/Notifier.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ReaderForLanguageLearners.Utils
+{
+    abstract class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
