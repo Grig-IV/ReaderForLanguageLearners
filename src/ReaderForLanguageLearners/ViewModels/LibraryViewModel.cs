405d8a113289ec234abab97e0dae6c64d0b1f0fe
Grig-IV <77640875+Grig-IV@users.noreply.github.com>
2021-09-21 12:50:00 +0300

    Implement LibraryViewModel

---
+++ src/ReaderForLanguageLearners/ViewModels/LibraryViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using ReaderForLanguageLearners.Interfaces;
+using ReaderForLanguageLearners.Models;
+using ReaderForLanguageLearners.Utils;
+using System.Collections.ObjectModel;
+
+namespace ReaderForLanguageLearners.ViewModels
+{
+    class LibraryViewModel
+    {
+        private ILibraryModel _libraryModel = LibraryModel.Instance;
+        private RelayCommand _addBookComand;
+
+        public ObservableCollection<IBookData> Books => _libraryModel.Books;
+
+        public RelayCommand AddBookComand
+        {
+            get => _addBookComand ??= new RelayCommand(_ =>
+            {
+                var openFileDialog = new OpenFileDialog
+                {
+                    Filter = "FB2 books (*.fb2)|*.fb2",
+                    Multiselect = true
+                };
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    foreach (string bookFilePath in openFileDialog.FileNames)
+                    {
+                        _libraryModel.AddBook(bookFilePath);
+                    }
+                }
+            });
+        }
+    }
+}
